0e2324d4e0a6bd0725fafea33b9b4dad8ef3ab92
Rein-Schregardus <rein@schre.nl>
2026-01-30 18:15:44 +0100

    swagger on docker
    Issues still remain around http handeling and swagger

--- backend/Server/Program.cs
+++ backend/Server/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
 });
 
 
@@ -18,11 +19,16 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("./v1/swagger.json", "My API V1"); //originally "./swagger/v1/swagger.json"
+});
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseDeveloperExceptionPage();
+
+app.MapSwagger();
 app.Run();
